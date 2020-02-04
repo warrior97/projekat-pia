3d68fa0646710ffe22dd09f6c8ab103ac09389e3
Dusan Bobicic <dbobicic@itekako.com>
2020-02-04 15:12:01 +0100

    Updated editor

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Question } from '../question';
 
 @Component({
   selector: 'app-editor',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
+  questions=[];
+  title:string;
+  description:string;
+  isTest:boolean;
   constructor() { }
 
   ngOnInit() {
   }
+  private addQuestion(type:string){
+    let ques=new Question();
+    ques.type=type;
+    this.questions.push(ques);
 
+  }
+
+  addQuestionNumber(){
+    this.addQuestion('number');
+  }
+  
+  addQuestionText(){
+    this.addQuestion('text');
+  }
+  addQuestionTextBig(){
+    this.addQuestion('textarea');
+  }
+  addQuestionSelect(){
+    this.addQuestion('select');
+  }
+  addQuestionCheckBox(){
+    this.addQuestion('checkbox');
+  }
+ 
 }
--- src/app/question.ts
+++ src/app/question.ts
@@ -2,8 +2,19 @@ export class Question {
     public name:string;
     public question:string;
     public type:string;
+    public answer:string
     public choices:Array<any>;
     public correct:Array<any>;
     public randomize:boolean;
     public points:number;
+
+
+    constructor(){
+        this.name='';
+        this.question='';
+        this.type='';
+        this.choices=[];
+        this.randomize=false;
+        this.points=0;
+    }
 }
